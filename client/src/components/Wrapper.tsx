29f7eab1cb56ecdfd3104a190ca6ff3d5172944a
Peter Cho <pcho51990@gmail.com>
2020-12-10 00:10:11 -0500

    added client

---
+++ client/src/components/Wrapper.tsx
@@ -0,0 +1,7 @@
+import Box from '@chakra-ui/core';
+
+interface WrapperProps {}
+
+export const Wrapper: React.FC<WrapperProps> = ({children}) => {
+  return <Box mt={8} mx="auto" maxW="800px" w="100%">{children}</Box>
+};
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,5 +1,5 @@
 import {MyContext} from '../types';
-import {Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver} from 'type-graphql';
+import {Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver} from 'type-graphql';
 import argon2 from 'argon2';
 import {User} from '../entities/User';
 
@@ -29,16 +29,49 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
-  async register(@Arg('options') options: UsernamePasswordInput, @Ctx() {em}: MyContext) {
-    const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {username: options.username, password: hashedPassword});
-    await em.persistAndFlush(user);
+  @Query(() => User, {nullable: true})
+  async me(@Ctx() {req, em}: MyContext) {
+    if (!req.session.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, {id: req.session.userId});
     return user;
   }
 
   @Mutation(() => UserResponse)
-  async login(@Arg('options') options: UsernamePasswordInput, @Ctx() {em}: MyContext): Promise<UserResponse> {
+  async register(@Arg('options') options: UsernamePasswordInput, @Ctx() {em}: MyContext): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [{field: 'username', message: 'length must be greater than 2'}]
+      };
+    }
+
+    if (options.password.length <= 3) {
+      return {
+        errors: [{field: 'password', message: 'length must be greater than 3'}]
+      };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {username: options.username, password: hashedPassword});
+
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        return {
+          errors: [{field: 'username', message: 'username has already been taken'}]
+        };
+      }
+      console.log('message: ', err.message);
+    }
+
+    return {user};
+  }
+
+  @Mutation(() => UserResponse)
+  async login(@Arg('options') options: UsernamePasswordInput, @Ctx() {em, req}: MyContext): Promise<UserResponse> {
     const user = await em.findOne(User, {username: options.username});
     if (!user) {
       return {
@@ -51,6 +84,9 @@ export class UserResolver {
         errors: [{field: 'password', message: 'invalid login'}]
       };
     }
+
+    req.session.userId = user.id;
+
     return {user};
   }
 }
